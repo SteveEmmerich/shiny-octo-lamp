7b4b6e175a3acb594012584e945d27b93fbb3fda
Steve Emmerich <sdemmer@Steves-MacBook-Pro.local>
2020-02-03 02:06:39 -0500

    Updates

---
+++ precache-manifest.3fbe6e462699932ff9c2a92dc74bcdd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6d25a6c6b5cd074ceb5d5ea57c56267",
+    "url": "/shiny-octo-lamp/index.html"
+  },
+  {
+    "revision": "654d6ddd253424d2d95d",
+    "url": "/shiny-octo-lamp/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "c419f301f08ceb86197f",
+    "url": "/shiny-octo-lamp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "654d6ddd253424d2d95d",
+    "url": "/shiny-octo-lamp/static/js/2.da9d72b7.chunk.js"
+  },
+  {
+    "revision": "b15a737ad2cfd16685fd27e8e85315d2",
+    "url": "/shiny-octo-lamp/static/js/2.da9d72b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c419f301f08ceb86197f",
+    "url": "/shiny-octo-lamp/static/js/main.978ffb48.chunk.js"
+  },
+  {
+    "revision": "69f2b915f4d420139359",
+    "url": "/shiny-octo-lamp/static/js/runtime-main.b8470b82.js"
+  }
+]);
